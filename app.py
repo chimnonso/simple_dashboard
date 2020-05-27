875bd23f408d4f78ddc7c0dfcecce6d3bf8c069c
Amaizu Gabriel <gabriel4amaizu@gmail.com>
2020-05-27 12:30:58 +0900

    flask-login

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from project import app
+
+
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
